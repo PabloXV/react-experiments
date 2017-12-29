170260f4bf8429ca1e61be68d790fcf33c30a4d0
= <pabloxvillalobos@gmail.com>
2017-12-28 20:36:20 -0500

    Finally set up react!

---
+++ app/components/Application.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Application extends React.Component {
+  render() {
+    return <h1>hello!</h1>;
+  }
+};
+
+export default Application;
---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Application from './components/Application';
+
+//<App />
+
+ReactDOM.render(
+  <Application />,
+  document.getElementById('app')
+);
